1bcf4fa8600b90e0aa5cc02914afce0879c95444
Huy Vo (Henry) <huyvo6812@gmail.com>
2023-05-23 02:38:24 +0700

    Update about-us page, overview project page, and product detail page. add-on dropdown for project description nav

--- javascript/about-us.js
+++ javascript/about-us.js
@@ -1,14 +1,54 @@
-function overlayShow() {
-    let popup = document.getElementById("overlay")
+function overlayShow_hanguyen() {
+    let popup = document.getElementById("hanguyen-overlay")
     let opacity = document.getElementById("container")
-    console.log(popup);
-    console.log(opacity);
     popup.classList.add("open-popup");
     opacity.classList.add("overlay-container");
 }
 
-function overlayHide() {
-    let popup = document.getElementById("overlay")
+function overlayShow_huyvo() {
+    let popup = document.getElementById("huyvo-overlay")
+    let opacity = document.getElementById("container")
+    popup.classList.add("open-popup");
+    opacity.classList.add("overlay-container");
+}
+
+function overlayShow_tramanh() {
+    let popup = document.getElementById("tramanh-overlay")
+    let opacity = document.getElementById("container")
+    popup.classList.add("open-popup");
+    opacity.classList.add("overlay-container");
+}
+
+function overlayShow_joshua() {
+    let popup = document.getElementById("joshua-overlay")
+    let opacity = document.getElementById("container")
+    popup.classList.add("open-popup");
+    opacity.classList.add("overlay-container");
+}
+
+function overlayHide_hanguyen() {
+    let popup = document.getElementById("hanguyen-overlay")
+    let opacity = document.getElementById("container")
+    popup.classList.remove("open-popup");
+    opacity.classList.remove("overlay-container");
+}
+
+function overlayHide_tramanh() {
+    let popup = document.getElementById("tramanh-overlay")
+    let opacity = document.getElementById("container")
+    popup.classList.remove("open-popup");
+    opacity.classList.remove("overlay-container");
+}
+
+function overlayHide_huyvo() {
+    let popup = document.getElementById("huyvo-overlay")
+    let opacity = document.getElementById("container")
+    popup.classList.remove("open-popup");
+    opacity.classList.remove("overlay-container");
+}
+
+function overlayHide_joshua() {
+    let popup = document.getElementById("joshua-overlay")
     let opacity = document.getElementById("container")
     popup.classList.remove("open-popup");
     opacity.classList.remove("overlay-container");
